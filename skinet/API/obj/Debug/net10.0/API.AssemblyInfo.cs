7212925ec64c5f402272fa1c678c2c9cf2e20869
Ushantha <ushathagreat@gmail.com>
2026-01-29 17:19:08 +0530

    Checked the server running port

--- skinet/API/obj/Debug/net10.0/API.AssemblyInfo.cs
+++ skinet/API/obj/Debug/net10.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+835d80264018121857be33029215a728caa26034")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fdd9de946f51889793810e4c70fb2ee102b0a768")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
