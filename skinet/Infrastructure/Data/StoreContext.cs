019897747ef04753c08be76bd3bd1ef765364d81
Ushantha <ushathagreat@gmail.com>
2026-01-31 11:34:25 +0530

    Updated StoreContext.cs
    for add 'Products' table into Database

--- skinet/Infrastructure/Data/StoreContext.cs
+++ skinet/Infrastructure/Data/StoreContext.cs
@@ -1,10 +1,11 @@
 using System;
+using Core.Entities;
 using Microsoft.EntityFrameworkCore;
 
 namespace Infrastructure.Data;
 
 public class StoreContext(DbContextOptions options) : DbContext(options)
 {
-    
+    public DbSet<Product> Products { get; set; }
     
 }
