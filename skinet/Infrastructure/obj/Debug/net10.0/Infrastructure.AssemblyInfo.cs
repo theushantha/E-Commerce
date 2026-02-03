b40c3fde855d0000277bee1a91233d7dcb416858
Ushantha <ushathagreat@gmail.com>
2026-02-03 16:49:05 +0530

    Implement the repository methods

--- skinet/Infrastructure/obj/Debug/net10.0/Infrastructure.AssemblyInfo.cs
+++ skinet/Infrastructure/obj/Debug/net10.0/Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bac613f32176ee953e0be0f6d03ff3ae67e59d4e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8f01c96998c8d69afd95ef835b66f39fd5d16df5")]
 [assembly: System.Reflection.AssemblyProductAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
