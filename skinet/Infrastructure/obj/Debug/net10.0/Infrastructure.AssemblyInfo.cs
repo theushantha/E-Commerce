eef8779b1add381be1240df7931edd050373e16b
Ushantha <ushathagreat@gmail.com>
2026-02-04 01:17:56 +0530

    Create StoreContextSeed.cs

--- skinet/Infrastructure/obj/Debug/net10.0/Infrastructure.AssemblyInfo.cs
+++ skinet/Infrastructure/obj/Debug/net10.0/Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c064638e37b999a81657af781a894514725917f8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4c5edfcaeaec2d2ac8066b8f4fe58fe88d14fba0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
