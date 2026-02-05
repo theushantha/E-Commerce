43b6de084ce0842854cf3e4e43bb9b2473b17c46
Ushantha <ushathagreat@gmail.com>
2026-02-05 21:17:28 +0530

    Create IGenericRepository Interface

--- skinet/API/obj/Debug/net10.0/API.AssemblyInfo.cs
+++ skinet/API/obj/Debug/net10.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f4df827eca892f31d5c764d88623a4ad6211b78f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b5ab5a357966ea7fe896d97aca03e9a03ff7b832")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
