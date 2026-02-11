26ac9e7330f545855b98c0f8b8bdab947738ba9e
Ushantha <ushathagreat@gmail.com>
2026-02-11 16:04:47 +0530

    Review the Angular project files

--- skinet/client/src/app/app.config.ts
+++ skinet/client/src/app/app.config.ts
@@ -1,11 +1,13 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import 'zone.js';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
+    provideRouter(routes),
+    provideZoneChangeDetection()
   ]
 };
