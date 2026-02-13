84081c3f19ce15a3577d642b9f5d3d7ef4a1c6c3
Ushantha <ushathagreat@gmail.com>
2026-02-13 15:56:40 +0530

    Add filters button

--- skinet/client/src/app/features/shop/shop.component.ts
+++ skinet/client/src/app/features/shop/shop.component.ts
@@ -3,18 +3,26 @@ import { ShopService } from '../../core/services/shop.service';
 import { Product } from '../../shared/models/product';
 import { MatCard } from '@angular/material/card';
 import { ProductItemComponent } from "./product-item/product-item.component";
+import { MatDialog } from '@angular/material/dialog';
+import { FiltersDialogComponent } from './filters-dialog/filters-dialog.component';
+import { MatButton } from '@angular/material/button';
+import { MatIcon } from '@angular/material/icon';
 
 @Component({
   selector: 'app-shop',
   imports: [
     MatCard,
-    ProductItemComponent
+    ProductItemComponent,
+    MatButton,
+    MatIcon
 ],
   templateUrl: './shop.component.html',
   styleUrl: './shop.component.scss',
 })
 export class ShopComponent implements OnInit {
   private shopService = inject(ShopService);
+  private dialogService = inject(MatDialog);
+
   products: Product[] = [];
 
   ngOnInit(): void {
@@ -29,4 +37,12 @@ export class ShopComponent implements OnInit {
       error: error => console.log(error)
     })
   }
+
+  openFiltersDialog() {
+    const dialogRef = this.dialogService.open(FiltersDialogComponent, {
+      minWidth: '500px'
+    })
+  }
+
+  
 }
