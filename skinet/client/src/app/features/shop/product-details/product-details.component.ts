4e1fe2a5fbdaf7b17f58e7e80a208caf1e8a753a
Ushantha <ushathagreat@gmail.com>
2026-02-19 00:12:17 +0530

    Get an individual product using Route params

--- skinet/client/src/app/features/shop/product-details/product-details.component.ts
+++ skinet/client/src/app/features/shop/product-details/product-details.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { ShopService } from '../../../core/services/shop.service';
+import { ActivatedRoute } from '@angular/router';
+import { Product } from '../../../shared/models/product';
 
 @Component({
   selector: 'app-product-details',
@@ -7,5 +10,22 @@ import { Component } from '@angular/core';
   styleUrl: './product-details.component.scss',
 })
 export class ProductDetailsComponent {
+  private shopService = inject(ShopService);
+  private activatedRoute = inject(ActivatedRoute);
+  product?: Product;
+  
+  ngOnInit(): void {
+    this.loadProduct();
+  }
 
+  loadProduct() {
+    const id = this.activatedRoute.snapshot.paramMap.get('id');
+    if (!id) return;
+    this.shopService.getProduct(+id).subscribe({
+      next: product => this.product = product,
+      error: error => console.log(error)
+    });
+  }
+
+  
 }
--- skinet/client/src/app/features/shop/product-item/product-item.component.ts
+++ skinet/client/src/app/features/shop/product-item/product-item.component.ts
@@ -1,10 +1,11 @@
-import { I } from '@angular/cdk/keycodes';
+import { I, R } from '@angular/cdk/keycodes';
 import { Component, Input } from '@angular/core';
 import { Product } from '../../../shared/models/product';
 import { MatCard, MatCardActions, MatCardContent } from '@angular/material/card';
 import { CurrencyPipe } from '@angular/common';
 import { MatButton } from '@angular/material/button';
 import { MatIcon } from "@angular/material/icon";
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-product-item',
@@ -14,7 +15,8 @@ import { MatIcon } from "@angular/material/icon";
     CurrencyPipe,
     MatCardActions,
     MatButton,
-    MatIcon
+    MatIcon,
+    RouterLink
 ],
   templateUrl: './product-item.component.html',
   styleUrl: './product-item.component.scss',
