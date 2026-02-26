dd6d0a32029607e0eb2b634169f6c3fd98065fba
Ushantha <ushathagreat@gmail.com>
2026-02-26 19:32:25 +0530

    Update the nav bar with the cart item count

--- skinet/client/src/app/layout/header/header.component.ts
+++ skinet/client/src/app/layout/header/header.component.ts
@@ -5,6 +5,8 @@ import { MatIcon } from '@angular/material/icon';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 import { BusyService } from '../../core/services/busy.service';
 import { MatProgressBar } from '@angular/material/progress-bar';
+import { Cart } from '../../shared/models/cart';
+import { CartService } from '../../core/services/cart.service';
 
 @Component({
   selector: 'app-header',
@@ -21,5 +23,6 @@ import { MatProgressBar } from '@angular/material/progress-bar';
 })
 export class HeaderComponent {
   busyService = inject(BusyService);
+  cartService = inject(CartService);
 
 }
