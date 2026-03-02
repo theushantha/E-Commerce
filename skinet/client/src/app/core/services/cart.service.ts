af1283683b39553e45541fe05d94a5257121586d
Ushantha <ushathagreat@gmail.com>
2026-03-02 18:32:47 +0530

    Add additional functions to the service

--- skinet/client/src/app/core/services/cart.service.ts
+++ skinet/client/src/app/core/services/cart.service.ts
@@ -51,7 +51,34 @@ export class CartService {
     }
     cart.items = this.addOrUpdateItem(cart.items, item, quantity);
     this.setCart(cart);
+  }
 
+  removeItemFromCart(productId: number, quantity = 1) {
+    const cart = this.cart();
+    if(!cart) return;
+    const index = cart.items.findIndex(x => x.productId === productId);
+    if (index !== -1) {
+      if (cart.items[index].quantity > quantity) {
+        cart.items[index].quantity -= quantity;
+      } else {
+        cart.items.splice(index, 1);
+      }
+      if (cart.items.length === 0) {
+        this.deleteCart();
+      } else {
+        this.setCart(cart);
+      }
+
+    }
+  }
+
+  deleteCart() {
+    this.http.delete(this.baseUrl + 'cart?id=' + this.cart()?.id).subscribe({
+      next: () => {
+        localStorage.removeItem('cart_id');
+        this.cart.set(null);
+      }
+    })
   }
 
   private addOrUpdateItem(items: CartItem[], item: CartItem, quantity: number): CartItem[] {
