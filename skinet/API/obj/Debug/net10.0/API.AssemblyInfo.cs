5ab55773dea585d827ef320ee8689536af2a15d5
Ushantha <ushathagreat@gmail.com>
2026-03-03 17:20:06 +0530

    Set up a cloud Redis Server

--- skinet/API/obj/Debug/net10.0/API.AssemblyInfo.cs
+++ skinet/API/obj/Debug/net10.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+43ef2bcad25d51b77e7b95729b1c6c728d260400")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+40240a23fd87131c09ea112798ecabc1280db901")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
