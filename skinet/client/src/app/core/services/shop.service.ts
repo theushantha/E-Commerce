43ef2bcad25d51b77e7b95729b1c6c728d260400
Ushantha <ushathagreat@gmail.com>
2026-03-03 15:45:45 +0530

    Prepare the client app for publishing

--- skinet/client/src/app/core/services/shop.service.ts
+++ skinet/client/src/app/core/services/shop.service.ts
@@ -3,12 +3,13 @@ import { inject, Injectable } from '@angular/core';
 import { Product } from '../../shared/models/product';
 import { Pagination } from '../../shared/models/pagination';
 import { ShopParams } from '../../shared/models/shopParams';
+import { environment } from '../../../environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ShopService {
-  baseUrl = 'https://localhost:5002/api/';
+  baseUrl = environment.apiUrl;
   private http = inject(HttpClient);
   types: string[] = [];
   brands: string[] = []; 
