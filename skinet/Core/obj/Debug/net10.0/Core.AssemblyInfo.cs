24b625a17489c029ab2d2fcc429e1896407f0fa6
Ushantha <ushathagreat@gmail.com>
2026-03-23 10:03:33 +0530

    Add Redis & Identity packages; update build outputs

--- skinet/Core/obj/Debug/net10.0/Core.AssemblyInfo.cs
+++ skinet/Core/obj/Debug/net10.0/Core.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c5b772077767874e353654ef583a91329ebc9343")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bf7a27a82fb46ec768ec7270a37f61b6aec6f8d9")]
 [assembly: System.Reflection.AssemblyProductAttribute("Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
