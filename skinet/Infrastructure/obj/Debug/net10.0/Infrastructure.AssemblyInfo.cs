a5524678513fb2e62cba0f4dbf1c39ec7bef8929
Ushantha <ushathagreat@gmail.com>
2026-03-23 10:22:55 +0530

    Add account components, service and routes

--- skinet/Infrastructure/obj/Debug/net10.0/Infrastructure.AssemblyInfo.cs
+++ skinet/Infrastructure/obj/Debug/net10.0/Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bf7a27a82fb46ec768ec7270a37f61b6aec6f8d9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+24b625a17489c029ab2d2fcc429e1896407f0fa6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
